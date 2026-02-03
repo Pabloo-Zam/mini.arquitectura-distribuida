ef1a8bf175455c81f61b6b7db35e924af91977d9
Pablo Zamora <PFZAMORA@Correo.url.edu.gt>
2026-02-03 15:12:37 -0600

    cambio en api/procesar

--- api/procesar.js
+++ api/procesar.js
@@ -1,7 +1,18 @@
 export default function handler(req, res) {
-  const nombre = req.query.nombre || "anónimo";
+  const nombre = (req.query.nombre || "anónimo").toString();
 
-  res.status(200).json({
-    resultado: `Nombre procesado: ${nombre.toUpperCase()}`
+ 
+  if (nombre.toLowerCase() === "error") {
+    return res.status(500).json({
+      error: "Error simulado en /api/procesar",
+      timestamp: new Date().toISOString()
+    });
+  }
+
+  
+  return res.status(200).json({
+    resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
+    timestamp: new Date().toISOString()
   });
 }
+
